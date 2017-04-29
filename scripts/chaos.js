1d29d6dcb280d482200624ca8843501d5faaef26
dagothar <dagothar@gmail.com>
2017-04-29 12:34:17 +0200

    some changes

--- scripts/chaos.js
+++ scripts/chaos.js
@@ -3,22 +3,44 @@
 var Chaos = (function() {
   
   /**
-   * @brief Chaos class constructor.
+   * @brief Point class constructor.
    */
-  function Chaos() {
-  }
-  
-
-  Chaos.prototype.update = function() {
-
+  function Point(x, y) {
+    this.x = x;
+    this.y = y;
   }
   
   
   /**
-   * @brief Renders the game state onto a canvas.
+   * @brief Chaos class constructor.
    */
-  Chaos.prototype.render = function(canvas) {
+  function Chaos(startPoint, points, stepSize) {
+    this._startPoint = startPoint;
+    this._points = points;
+    this._stepSize = stepSize;
+    this._current = this._startPoint;
+  }
+  
+  
+  Chaos.prototype.setStartPoint   = function(startPoint)  { this._startPoint = startPoint; }
+  Chaos.prototype.setPoints       = function(points)      { this._points = points; }
+  Chaos.prototype.addPoint        = function(point)       { this._points.push(point); }
+  Chaos.prototype.clearPoints     = function()            { this._points = []; }
+  
+
+  /**
+   * @brief Returns new Chaos Game point.
+   */
+  Chaos.prototype.update = function(context) {
+    /* pick the new target */
+    var target = this._points[Math.floor(Math.random() * this._points.length)];
     
+    /* calculate new position */
+    var x = (1.0-this._stepSize) * _current.x + this._stepSize * target.x;
+    var y = (1.0-this._stepSize) * _current.y + this._stepSize * target.y;
+    this._current = new Point(x, y);
+    
+    return this._current;
   }
   
   
